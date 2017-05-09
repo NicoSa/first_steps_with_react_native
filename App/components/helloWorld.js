1a16c1e3523896acc4fb281b743b5ab3a5966586
Nico Saueressig <github@nicosaueressig.de>
2017-05-09 16:32:28 +0200

    init

---
+++ App/components/helloWorld.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+import {
+   StyleSheet,
+   Text,
+   View,
+   Platform
+} from 'react-native'
+
+const HelloWorld = (props) => {
+  if (Platform.OS === "ios") {
+    return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        "Hello iOS World!"
+      </Text>
+    </View>
+    )
+  } else if (Platform.OS === "android") {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          "Hello Android World"
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default HelloWorld
---
+++ index.ios.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import HelloWorld from './App/components/helloWorld'
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
